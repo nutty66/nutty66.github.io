45e411bcdb6af5a9f623e2c5a05f983068499bb7
nutty66 <159878197+nutty66@users.noreply.github.com>
2024-02-22 20:40:55 +0700

    Create steamlit02.py

---
+++ steamlit02.py
@@ -0,0 +1,8 @@
+import streamlit as st
+print(st.__version__)
+t.sidebar.text("Sidebar")
+st.sidebar.button("หน้าแรก")
+st.sidebar.button("สารบัญ")
+st.sidebar.button("สรุป")
+st.title('This is a title')
+st.title('_Streamlit_ is :blue[cool] :sunglasses:')
