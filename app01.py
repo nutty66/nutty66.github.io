51f0cca117da4074c6fe37925c3818036f197a6c
nutty66 <159878197+nutty66@users.noreply.github.com>
2024-02-22 20:46:10 +0700

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.header("My OOP")
+
+st.sidebar.text("Sidebar")
+st.sidebar.button("หน้าแรก")
+st.sidebar.button("สารบัญ")
+st.sidebar.button("สรุป")
+st.title('This is a title')
+st.title('_Streamlit_ is :blue[cool] :sunglasses:')
+Copy
